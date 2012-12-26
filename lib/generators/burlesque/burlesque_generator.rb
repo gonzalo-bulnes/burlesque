867bf951d4d9d505c23bfc624c1042a865adf071
mespina <mespina.icc@gmail.com>
2012-12-26 15:24:36 -0300

    fix instalador

--- lib/generators/burlesque/burlesque_generator.rb
+++ lib/generators/burlesque/burlesque_generator.rb
@@ -1,9 +1,9 @@
-require 'rails/generators/migration'
+require 'rails/generators/active_record'
 require 'thor/shell/basic.rb'
 
-module Burlesque
+module ActiveRecord
   module Generators
-    class BurlesqueGenerator < Rails::Generators::Base
+    class BurlesqueGenerator < ActiveRecord::Generators::Base
       argument :attributes, :type => :array, :default => []
 
       def inject_burlesque_content
@@ -11,8 +11,18 @@ module Burlesque
   include Burlesque::Admin
 CONTENT
 
+        class_path = if namespaced?
+          class_name.to_s.split("::")
+        else
+          [class_name]
+        end
+
+        indent_depth = class_path.size - 1
+        content = content.split("\n").map { |line| "  " * indent_depth + line } .join("\n") << "\n"
+
+
         puts "#{attributes.inspect}"
-        inject_into_class(File.join("app", "models", "#{class_name}.rb"), class_name, content)
+        inject_into_class(model_path, class_path.last, content) if File.exists?(File.join(destination_root, model_path))
       end
     end
   end
