1a8f7939213e144b6fc0b541565402c9e8e8b18e
mespina <mespina.icc@gmail.com>
2012-12-26 12:26:08 -0300

    try locale

--- lib/burlesque.rb
+++ lib/burlesque.rb
@@ -4,3 +4,12 @@
 require "burlesque/role"
 # require "burlesque/role_group"
 require "burlesque/version"
+
+module Burlesque
+  class Railtie < ::Rails::Railtie #:nodoc:
+    initializer 'rails-i18n' do |app|
+      I18n.load_path << Dir[File.join(File.expand_path(File.dirname(__FILE__) + '/../config/locales'), '*.yml')]
+      I18n.load_path.flatten!
+    end
+  end
+end
