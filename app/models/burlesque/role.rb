b96927efde84b0b11307402f2f28b7d48c0b62a9
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2013-11-08 17:33:48 -0300

    Fix undefined methos `underscore` for ActiveModel
    See https://github.com/lassebunk/dynamic_sitemaps/pull/12

--- app/models/burlesque/role.rb
+++ app/models/burlesque/role.rb
@@ -81,11 +81,11 @@ module Burlesque
     # Mejorar el retorno, para saber que paso, ej: si se agregaron los 5 roles o si ya existen
     def self.for model
       if model.class == String
-        resource = model.classify.constantize.model_name.underscore
+        resource = model.classify.constantize.model_name.to_s.underscore
       elsif model.class == Symbol
-        resource = model.to_s.classify.constantize.model_name.underscore
+        resource = model.to_s.classify.constantize.model_name.to_s.underscore
       elsif model.class == Class
-        resource = model.model_name.underscore
+        resource = model.model_name.to_s.underscore
       end
 
       if resource
